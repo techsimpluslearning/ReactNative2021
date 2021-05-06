d2e8bbf27c0c182e1710582d18bbd319c5172058
Prateek Mishra <p@g.com>
2021-05-06 07:07:27 +0530

    First React Native App

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        flexDirection: 'row',
+        alignItems: 'center',
+      }}>
+      <Text>Hello to all....</Text>
+    </View>
+  );
+};
+
+export default App;
