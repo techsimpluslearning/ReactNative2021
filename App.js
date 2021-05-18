cff0b0ae9b5db700e7370b1f9e67a5da353cf05f
Prateek Mishra <p@g.com>
2021-05-18 07:32:56 +0530

    UI Design Complete

--- App.js
+++ App.js
@@ -8,9 +8,11 @@ import {
   StyleSheet,
   ImageBackground,
   StatusBar,
+  Dimensions,
 } from 'react-native';
 
 const App = () => {
+  const {width, height} = Dimensions.get('screen');
   return (
     <View style={Styles.contaner}>
       <StatusBar translucent backgroundColor="transparent" />
@@ -30,6 +32,60 @@ const App = () => {
             source={require('./src/assets/images/3.png')}
           />
         </View>
+
+        <View style={Styles.cityDateContainer}>
+          <View>
+            <Text style={Styles.cityText}>Bhopal</Text>
+          </View>
+          <View>
+            <Text style={Styles.dateText}>Tue, 18 May, 2021</Text>
+          </View>
+        </View>
+
+        <View style={Styles.tempContainer}>
+          <View>
+            <Text style={Styles.tempText}>32</Text>
+          </View>
+          <View
+            style={{
+              flex: 1,
+              flexDirection: 'row',
+              alignItems: 'center',
+              marginTop: -25,
+            }}>
+            <View>
+              <Image
+                style={{width: 60, height: 60}}
+                source={{
+                  uri: 'https://icons.iconarchive.com/icons/icons-land/weather/256/Sunny-icon.png',
+                }}
+              />
+            </View>
+            <View>
+              <Text style={{color: 'white', fontSize: 30}}>Sunny</Text>
+            </View>
+          </View>
+        </View>
+
+        <View style={[Styles.border, {width: width - 30}]} />
+
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-around',
+            marginRight: 100,
+            bottom: 200,
+          }}>
+          <View>
+            <Text style={Styles.optionText}>Wind</Text>
+          </View>
+          <View>
+            <Text style={Styles.optionText}>Rain</Text>
+          </View>
+          <View>
+            <Text style={Styles.optionText}>Humidity</Text>
+          </View>
+        </View>
       </View>
     </View>
   );
@@ -38,10 +94,10 @@ const App = () => {
 const Styles = StyleSheet.create({
   contaner: {flex: 1, marginTop: -5},
   bgImage: {width: '100%', height: '105%'},
-  contentContainer: {flex: 1, position: 'absolute'},
+  contentContainer: {flex: 1, position: 'absolute', height: '100%'},
   header: {
     width: '76%',
-    top: 50,
+    marginTop: 60,
     paddingHorizontal: 20,
     flex: 1,
     justifyContent: 'space-between',
@@ -51,6 +107,26 @@ const Styles = StyleSheet.create({
     width: 25,
     height: 25,
   },
+  cityDateContainer: {
+    marginTop: 150,
+    position: 'absolute',
+    paddingHorizontal: 20,
+  },
+  cityText: {color: 'white', fontSize: 52},
+  dateText: {color: 'white', fontSize: 20},
+  tempContainer: {
+    position: 'absolute',
+    bottom: 260,
+    paddingHorizontal: 20,
+  },
+  tempText: {color: 'white', fontSize: 130},
+  border: {
+    height: 3,
+    backgroundColor: 'gray',
+    bottom: 230,
+    marginHorizontal: 20,
+  },
+  optionText: {color: 'white', fontSize: 20},
 });
 
 export default App;
